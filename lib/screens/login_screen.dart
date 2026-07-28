68b1752937761e50b0e42a208785518d4d6c4678
ajmm010 <angelon010@gmail.com>
2026-07-28 17:27:00 -0600

    add succes/fail triggers with email and password regex validation

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -10,7 +10,6 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-
   //Control para mostrar/ocultar contraseña
   bool _obscureText = true;
 
@@ -22,22 +21,75 @@ class _LoginScreenState extends State<LoginScreen> {
   SMITrigger? _trigSuccess;
   SMITrigger? _trigFail;
 
-  //3.1 Varibales para recorrerla mirada 
+  //3.1 Varibales para recorrerla mirada
   SMINumber? _numLook;
 
   //4.2 timer oara detener la mirada al dejar de escribir
   Timer? _typingDebbunce;
 
+  //5.1 Controles para manipular el texto escrito
+  final _emailController = TextEditingController();
+  final _passController = TextEditingController();
+
+  //5.2 Errores para mostrar en la UI
+  String? _emailError;
+  String? _passError;
+
   //2.1 Crear variables para focusNode
   final _emailFocus = FocusNode();
   final _passwordFocus = FocusNode();
 
+  //5.3 Validadores
+  bool isValidEmail(String email) {
+    final regularExp = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+    return regularExp.hasMatch(email);
+  }
+
+  bool isValidPassword(String pass) {
+    final regularExp = RegExp(
+      r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d]).{8,}$',
+    );
+    return regularExp.hasMatch(pass);
+  }
+
+  //5.4 Acción al botón de login
+  void _onLogin() {
+    // De lo que escribió el usuario, quitar los espacios en blanco al inicio y al final
+    final email = _emailController.text.trim();
+    final pass = _passController.text.trim();
+
+    //Reclutar emailError
+    final emailMsgError = isValidEmail(email) ? null : 'Email inválido';
+    final passMsgError = isValidPassword(pass)
+        ? null
+        : " 8 carcateres min, 1mayus, 1 minus, 1 número y carcater especial";
+
+    // 5.5 Notificar cambios
+    setState(() {
+      _emailError = emailMsgError;
+      _passError = passMsgError;
+    });
+
+    //5.6 Cerrar el teclado y bajar las manos
+    FocusScope.of(context).unfocus();
+    _typingDebbunce?.cancel();
+    _isChecking?.change(false);
+    _isHandsUp?.change(false);
+    _numLook?.value = 50.0;
+
+    //5.7 Activar los Trigger
+    if (emailMsgError == null && passMsgError == null) {
+      _trigSuccess?.fire();
+    } else {
+      _trigFail?.fire();
+    }
+  }
 
   @override
   void initState() {
     super.initState();
     // 2.2 Listeners (Oyentes/Chismosos)
-    _emailFocus.addListener((){
+    _emailFocus.addListener(() {
       if (_emailFocus.hasFocus) {
         // No tapes los ojos al ver email
         if (_isHandsUp != null) {
@@ -47,7 +99,7 @@ class _LoginScreenState extends State<LoginScreen> {
         }
       }
     });
-    _passwordFocus.addListener((){
+    _passwordFocus.addListener(() {
       //Mano arriba en password
       _isHandsUp?.change(_passwordFocus.hasFocus);
     });
@@ -91,6 +143,8 @@ class _LoginScreenState extends State<LoginScreen> {
               const SizedBox(height: 10),
               // campo de texto para el correo electrónico
               TextField(
+                //5.8 Enlazar controles
+                controller: _emailController,
                 focusNode: _emailFocus,
                 //1.3 vincular SMI´s a inputs de UI
                 onChanged: (value) {
@@ -125,7 +179,9 @@ class _LoginScreenState extends State<LoginScreen> {
                 // para mostrar un tipo de teclado
                 keyboardType: TextInputType.emailAddress,
                 decoration: InputDecoration(
+                  //5.9 Mostrar el texto de error
                   hintText: 'email',
+                  errorText: _emailError,
                   prefixIcon: const Icon(Icons.email),
                   border: OutlineInputBorder(
                     //Parametro borde redondeado
@@ -136,6 +192,8 @@ class _LoginScreenState extends State<LoginScreen> {
               const SizedBox(height: 10),
               // campo de texto para la contraseña
               TextField(
+                //5.8 Enlazar controles
+                controller: _passController,
                 focusNode: _passwordFocus,
                 //1.4 vincular SMI´s a inputs de UI
                 onChanged: (value) {
@@ -151,6 +209,7 @@ class _LoginScreenState extends State<LoginScreen> {
                 obscureText: _obscureText,
                 decoration: InputDecoration(
                   hintText: 'password',
+                  errorText: _passError,
                   prefixIcon: const Icon(Icons.lock),
                   suffixIcon: IconButton(
                     icon: Icon(
@@ -167,15 +226,37 @@ class _LoginScreenState extends State<LoginScreen> {
                   ),
                 ),
               ),
+              const SizedBox(height: 10),
+              MaterialButton(
+                //Toma todo el ancho posible
+                minWidth: double.infinity,
+                height: 50,
+                color: Colors.blueAccent,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ), //RounderRectanleBorder
+                onPressed: _onLogin,
+                child: const Text(
+                  'Login',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
             ],
           ),
         ),
       ),
     );
   }
+
   //2.4 Libera recursos de la memoria
   @override
   void dispose() {
+    //5.10 Liberar Controles
+    _emailController.dispose();
+    _passController.dispose();
     _emailFocus.dispose();
     _passwordFocus.dispose();
     _controller?.dispose();
